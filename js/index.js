47ea5b3c2863b3b25494c4d0e6e31504e023876b
palmerito0 <sandiegobutterflies@gmail.com>
2016-09-30 21:18:38 +0000

    Removed text

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-function easySound () {
-    var audio = new Audio('easy.mp3');
+var audio;
+
+$(document).ready(function() {
+    audio = new Audio('easy.mp3');
+});
+
+$("#easy").on("click", function(){
     audio.play();
-}
+});
