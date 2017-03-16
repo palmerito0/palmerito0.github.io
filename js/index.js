2ba161f220f697a060fa5236717a88587f53c6ec
palmerito0 <sandiegobutterflies@gmail.com>
2017-03-16 16:54:55 +0000

    Moved old site into Old Site folder, added distance finder example

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var audio;
-
-$(document).ready(function() {
-    audio = new Audio('../assets/easy.mp3');
-});
-
-$("#easy").on("click", function(){
-    audio.play();
-});
--- js/twilio.js
+++
@@ -1,5 +0,0 @@
-function twilio(form){
-    console.log("SUCCESS!")
-    var phoneNumber = form.phoneNumber.value;
-    Twilio.callAndPlay(phoneNumber, "http://palmerito0.github.io/johncena.mp3");
-}
